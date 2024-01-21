df15048552ed2d1c2373381752a6050601ca617c
giucino <125276491+giucino@users.noreply.github.com>
2024-01-21 13:31:07 +0100

    Update dependencies and file paths

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimations(), provideAnimations()]
+  providers: [provideRouter(routes), provideClientHydration(), provideAnimations(), provideAnimations(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"ring-of-fire-5c18b","appId":"1:960039331396:web:f4622efffad4cc85ba3e11","storageBucket":"ring-of-fire-5c18b.appspot.com","apiKey":"AIzaSyATmSX39COydJmAIvE5tqfUnDIOSnp6CeA","authDomain":"ring-of-fire-5c18b.firebaseapp.com","messagingSenderId":"960039331396"}))), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideDatabase(() => getDatabase()))]
 };
